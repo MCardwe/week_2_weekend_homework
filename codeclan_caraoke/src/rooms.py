0ddf58957561cb8f7ae7bc037230cd1f968e51ed
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-04 15:25:13 +0000

    Finished and tested check in and check out

--- codeclan_caraoke/src/rooms.py
+++ codeclan_caraoke/src/rooms.py
@@ -3,5 +3,11 @@
 class Room:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.occupied = []
+        self.guests_in_room = []
         self.songs = []
+
+    def check_in(self, guest):
+        self.guests_in_room.append(guest)
+
+    def check_out(self):
+        self.guests_in_room.clear()
--- codeclan_caraoke/tests/rooms_tests.py
+++ codeclan_caraoke/tests/rooms_tests.py
@@ -1,5 +1,7 @@
 import unittest
 from src.rooms import Room
+from src.guests import Guest
+from src.songs import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
@@ -11,7 +13,21 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(1, self.room_1.capacity)
 
     def test_no_occupancy_in_rooms(self):
-        self.assertEqual(0, len(self.room_1.occupied))
+        self.assertEqual(0, len(self.room_1.guests_in_room))
 
     def test_room_has_no_songs(self):
         self.assertEqual(0, len(self.room_1.songs))
+
+    def test_check_in_guest(self):
+        
+        guest = Guest("Bill", "Location - Khalid", 25)
+        self.room_1.check_in(guest)
+        self.assertEqual(1, len(self.room_1.guests_in_room))
+
+    def test_check_out(self):
+        guest = Guest("Bill", "Location - Khalid", 25)
+        self.room_1.check_in(guest)
+        self.room_1.check_out()
+        self.assertEqual(0, len(self.room_1.guests_in_room))
+
+    def 
